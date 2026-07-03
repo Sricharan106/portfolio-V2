4ed1db84cc14ae3171d1305ec8e6768ca6b88340
Sricharan106 <anishetyswapna4@gmail.com>
2026-07-03 22:45:54 +0530

    chore: added sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import VitePluginSitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     allowedHosts: ["*"],
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePluginSitemap({
+      hostname: "https://parsisricharan.qzz.io",
+    }),
+  ],
 });
